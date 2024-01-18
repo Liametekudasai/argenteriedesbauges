bab8c54daab0fd4d989e729f9688d6f6099f27ea
liametekudasai <liamtom.rossi@gmail.com>
2024-01-18 15:23:35 +0100

    réorganisation des fichiers du site ainsi que premier test de fond d'écran

---
+++ PHP/article_echo_des_copeaux.php
@@ -0,0 +1,6 @@
+<!--_______________________________________________________Menu déroulant______________________________________________________-->
+<?php include "../HTML/barre_de_menu.html"?>
+<!--____________________________________________text de l'article de l'echo des copeaux________________________________________-->
+<?php include "../HTML/article_echo_des_copeaux.html"?>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -3,12 +3,12 @@
     <head>
         <meta charset="utf-8"> <!--donne la forme de language toujours utf-8-->
         <title>accueil - site de l'argenterie des bauges</title>
-        <link rel="stylesheet" href="CSS/etapes_historiques_layout.css"/><!-- href="nom du dossier css.css"-->
+        <link rel="stylesheet" href="CSS/index_layout.css"/><!-- href="nom du dossier css.css"-->
         <link rel="stylesheet" href="CSS/general_layout.css"/>
     </head>
     <body>
 <!--_______________________________________________________Menu déroulant______________________________________________________-->
-<?php include "barre_de_menu.html"?>
+<?php include "HTML/barre_de_menu.html"?>
 <!--____________________________________________corps________________________________________-->
     <h1>Bienvenue sur le site de l'Argenterie des Bauges</h1>
         <div class="container">
@@ -19,7 +19,7 @@
                 </div>
             </div>
         </div>
-    <?php include "counter.html"?>
+    <?php include "HTML/counter.html"?>
     <p>
         site mis a jour le 22/06/2023
     </p>
