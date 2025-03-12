39dbf1f9a72e254b15222973d45d72f9f7587ae2
Liametekudasai <123362603+Liametekudasai@users.noreply.github.com>
2025-03-12 22:57:22 +0000

    fin de dev du site avec l'aide d'une IA

--- PHP/article_echo_des_copeaux.php
+++ PHP/article_echo_des_copeaux.php
@@ -2,5 +2,3 @@
 <?php include "../HTML/barre_de_menu.html"?>
 <!--____________________________________________text de l'article de l'echo des copeaux________________________________________-->
 <?php include "../HTML/article_echo_des_copeaux.html"?>
-    </body>
-</html>
--- index.php
+++ index.php
@@ -1,29 +1,29 @@
-<!DOCTYPE html> <!--obligatoire pour l'écriture HTML-->
-<html> <!--obligatoire pour l'écriture HTML-->
+<!DOCTYPE html>
+<html lang="fr">
     <head>
-        <meta charset="utf-8"> <!--donne la forme de language toujours utf-8-->
-        <title>accueil - site de l'argenterie des bauges</title>
-        <link rel="stylesheet" href="CSS/index_layout.css"/><!-- href="nom du dossier css.css"-->
-        <link rel="stylesheet" href="CSS/general_layout.css"/>
-    </head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Site officiel de l'Argenterie des Bauges - Découvrez l'histoire et le savoir-faire du tournage à la perche">
+        <title>Accueil - L'Argenterie des Bauges</title>
+        <link rel="stylesheet" href="CSS/general_layout.css">
+        <link rel="stylesheet" href="CSS/index_layout.css">
     <body>
-<!--_______________________________________________________Menu déroulant______________________________________________________-->
-<?php include "HTML/barre_de_menu.html"?>
-<!--____________________________________________corps________________________________________-->
-    <h1>Bienvenue sur le site de l'Argenterie des Bauges</h1>
-        <div class="container">
-            <div class="col">
-                <div class="row">
-                    <figure><img src="assets/alvrule.gif" class="imgContainer"><figcaption></figcaption></figure>
-                    
-                </div>
-                <div class="row">
-                    <p>bonjour et bienvenue sur le site de l'argenterie des bauges, Ici vous pouvez consulter toute sorte de documents historiques sur l'histoire de l'argenterie des bauages, et si vous etes interessés vous pouvez aussi m'appeler pour visiter mon atelier et assisster a une démonstration de tournage en direct</p>
-                    
-                </div>
+    <?php include "HTML/barre_de_menu.html"?>
+    
+    <main class="container">
+        <h1>Bienvenue sur le site de l'Argenterie des Bauges</h1>
+        <div class="col">
+            <div class="row">
+                <figure>
+                    <img src="assets/alvrule.gif" class="imgContainer" alt="Séparateur décoratif">
+                </figure>
+            </div>
+            <div class="row">
+                <p>Bienvenue sur le site de l'Argenterie des Bauges. Ici, vous pouvez consulter toutes sortes de documents historiques sur l'histoire de l'Argenterie des Bauges. Si vous êtes intéressé, vous pouvez également me contacter pour visiter mon atelier et assister à une démonstration de tournage en direct.</p>
             </div>
         </div>
-    <p>
-        site mis a jour le 06/11/2024
-    </p>
+        <footer>
+            <p>Site mis à jour le 12/03/2025</p>
+        </footer>
+    </main>
 </body>
